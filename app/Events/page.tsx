ac9da153885dab1818f53a693a3ef2e26ac5fb13
Juiceee <lbehr@student.42.fr>
2024-09-24 17:23:26 +0200

    change layout + add event + contact

---
+++ app/Events/page.tsx
@@ -0,0 +1,5 @@
+export default function Events() {
+	return (
+		<h1 className="text-3xl">Events</h1>
+	);
+}
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,25 +1,28 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Link } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export default function Nav() {
+	const router = useRouter();
 	return (
 		<div className="w-full flex items-center justify-center px-8 py-4 mx-auto bg-transparent fixed top-0 left-0 z-50">
 			<div className="flex items-center justify-between w-full">
 				<div>
-					<Link href="/" >
+					<button onClick={() => router.push("/")}>
 						<Image src="/42 Blockchain Blanc.svg" alt="42 Blockchain" width={200} height={200} />
-					</Link>
+					</button>
 				</div>
 				<div className="flex justify-between gap-8">
-					<Link href="#">
+					<button onClick={() => router.push("./Events")}>
 						Events
-					</Link>
-					<Link color="foreground" href="#">
+					</button>
+					<button onClick={() => router.push("./Contact")}>
 						Contact
-					</Link>
+					</button>
 				</div>
 			</div>
-		</div>
+		</div >
 	);
 }
